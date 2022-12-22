63c91ca6171a56530d225fc425fe165055041271
B1ll-C <111455157+B1ll-C@users.noreply.github.com>
2022-12-22 22:36:55 +0800

    doc_pnl(Updated Design)

--- System/Test/Doc_pnl.java
+++ System/Test/Doc_pnl.java
@@ -36,6 +36,8 @@ public class Doc_pnl extends JPanel{
 		JPanel pnl = new JPanel();
 		JPanel pnl_left = new JPanel();
 		JPanel pnl_main = new JPanel();
+		JPanel pnl_left_main = new JPanel();
+		JPanel pnl_top = new JPanel();	
 
 		// JTextPane tbx_num = new JTextPane();
 		// JTextPane tbx_name = new JTextPane();
@@ -51,12 +53,15 @@ public class Doc_pnl extends JPanel{
 		JLabel lbl_name = new JLabel("Name");
 		JLabel lbl_mail = new JLabel("Email");
 		JLabel lbl_m_y = new JLabel("Month Year");
-		JLabel lbl_cont = new JLabel("Contact#");
+		JLabel lbl_cont = new JLabel("Contact no.");
 
 		JButton btn_add = new JButton("Add");
 		JButton btn_del = new JButton("Delete");
 		JButton btn_update = new JButton("Update");
 
+
+		JLabel lbl_title = new JLabel("Doctors");
+		JLabel lbl_title1 = new JLabel("Accounts");
 
 
 
@@ -66,83 +71,114 @@ public class Doc_pnl extends JPanel{
 		pnl.setBackground(Color.WHITE);
 
 
+
+
+
 		pnl_left.setAlignmentY(Component.TOP_ALIGNMENT);
 		pnl_left.setAlignmentX(Component.LEFT_ALIGNMENT);
-		pnl_left.setBackground(Color.RED);
-		pnl_left.setBounds(0,0,300,646-50);
+		// pnl_left.setBackground(new Color(211, 227, 239));
+		pnl_left.setBackground(Color.WHITE);
+		pnl_left.setBounds(10,10,300-20,646-70);
 		pnl_left.setLayout(null);
 		pnl.add(pnl_left);
+
+
+		lbl_title.setFont(new Font("Barlow Condensed", Font.BOLD, 30));
+		lbl_title.setForeground(Color.BLACK);
+		lbl_title.setBounds(10,10,300,33);
+		pnl_left.add(lbl_title);
+
+		lbl_title1.setFont(new Font("Barlow Condensed", Font.BOLD, 30));
+		lbl_title1.setForeground(Color.BLACK);
+		lbl_title1.setBounds(10,33+10,300,33);
+		pnl_left.add(lbl_title1);
+
+
+		pnl_left_main.setBounds(0,140,280,646-70-140);
+		pnl_left_main.setLayout(null);
+		pnl_left_main.setBackground(new Color(211, 227, 239));
+		pnl_left_main.setBorder(BorderFactory.createMatteBorder(2,2,2,2,new Color(0, 137, 143)));
+
+		pnl_left.add(pnl_left_main);
 
 	
 
 
 		int h = 100;
-		lbl_num.setBounds(0,20,150,30);
+		lbl_num.setBounds(10,0,150,30);
 		lbl_num.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_num.setBounds(80,50,150,30);
+		tbx_num.setBounds(80,35,150,30);
 
-		lbl_name.setBounds(0,100,150,30);
+		lbl_name.setBounds(10,80,150,30);
 		lbl_name.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_name.setBounds(80,130,150,30);
+		tbx_name.setBounds(80,80+35,150,30);
 
-		lbl_mail.setBounds(0,180,150,30);
+		lbl_mail.setBounds(10,160,150,30);
 		lbl_mail.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_mail.setBounds(80,210,150,30);
+		tbx_mail.setBounds(80,160+35,150,30);
 
-		lbl_m_y.setBounds(0,260,150,30);
+		lbl_m_y.setBounds(10,240,150,30);
 		lbl_m_y.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_m_y.setBounds(80,290,150,30);
+		tbx_m_y.setBounds(80,240+35,150,30);
 
-		lbl_cont.setBounds(0,340,150,30);
+		lbl_cont.setBounds(10,320,150,30);
 		lbl_cont.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_cont.setBounds(80,370,150,30);
+		tbx_cont.setBounds(80,320+35,150,30);
 
 		int x = 80;
-		lbl_filt.setBounds(0,340+x+80,150,30);
-		lbl_filt.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
-		tbx_filt.setBounds(80,450+x,150,30);
+		// lbl_filt.setBounds(0,400+x+80,150,30);
+		// lbl_filt.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
+		tbx_filt.setBounds(20,20,150,30);
+		tbx_filt.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
+		tbx_filt.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
 
 
 
-		pnl_left.add(lbl_filt);
-		pnl_left.add(tbx_filt);
+		pnl_top.add(lbl_filt);
+		pnl_top.add(tbx_filt);
 
 
-		pnl_left.add(tbx_num);
-		pnl_left.add(lbl_num);
+		pnl_left_main.add(tbx_num);
+		pnl_left_main.add(lbl_num);
 
-		pnl_left.add(tbx_name);
-		pnl_left.add(lbl_name);
+		pnl_left_main.add(tbx_name);
+		pnl_left_main.add(lbl_name);
 
-		pnl_left.add(tbx_mail);
-		pnl_left.add(lbl_mail);
+		pnl_left_main.add(tbx_mail);
+		pnl_left_main.add(lbl_mail);
 
-		pnl_left.add(tbx_m_y);
-		pnl_left.add(lbl_m_y);
+		pnl_left_main.add(tbx_m_y);
+		pnl_left_main.add(lbl_m_y);
 
-		pnl_left.add(tbx_cont);
-		pnl_left.add(lbl_cont);
-
-
-
-		btn_add.setBounds(0,450,100,33);
+		pnl_left_main.add(tbx_cont);
+		pnl_left_main.add(lbl_cont);
 
 
 
-		btn_del.setBounds(0+100,450,100,33);
+		btn_add.setBounds(10,100,80,33);
+		btn_add.setBackground(new Color(211, 227, 239));
+		btn_add.setBorder(BorderFactory.createMatteBorder(2,2,2,2,new Color(0, 137, 143)));
 
+		btn_del.setBounds(10+90,100,80,33);
+		btn_del.setBackground(new Color(211, 227, 239));
+		btn_del.setBorder(BorderFactory.createMatteBorder(2,2,2,2,new Color(0, 137, 143)));
 
-
-		btn_update.setBounds(0+200,450,100,33);
+		btn_update.setBounds(10+180,100,80,33);
+		btn_update.setBackground(new Color(211, 227, 239));
+		btn_update.setBorder(BorderFactory.createMatteBorder(2,2,2,2,new Color(0, 137, 143)));
 
 		pnl_left.add(btn_add);
 		pnl_left.add(btn_del);
 		pnl_left.add(btn_update);
 
 		
+		pnl_top.setBounds(300,10,1054-500-10,70);
+		pnl_top.setBackground(Color.WHITE);
+		pnl_top.setLayout(null);
+		pnl.add(pnl_top);
 
 		pnl_main.setLayout(new GridLayout(0,1));
-		pnl_main.setBounds(300,0,1054-500,646-50);
+		pnl_main.setBounds(300,80,1054-500-10,646-120-20);
 		pnl_main.setBackground(Color.GREEN);
 		pnl.add(pnl_main);
 
@@ -154,6 +190,7 @@ public class Doc_pnl extends JPanel{
 
 	 	DefaultTableModel model = new DefaultTableModel();
 		JTable tab = new JTable(model);
+		// tab.setBorder(BorderFactory.createMatteBorder(2,2,2,2,new Color(0, 137, 143)));
 		model.addColumn("Number");
 		model.addColumn("Name");
 		model.addColumn("Email");
@@ -260,6 +297,16 @@ public class Doc_pnl extends JPanel{
 				   tab.setRowSorter(sorter);
 					// final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
 				}
+			}
+		});
+
+		tbx_filt.addFocusListener(new FocusAdapter(){
+			public void focusGained(FocusEvent e){
+				tbx_filt.setText("");
+			}
+
+			public void focusLost(FocusEvent e){
+				tbx_filt.setText("Search: ");
 			}
 		});
 		return pnl;
