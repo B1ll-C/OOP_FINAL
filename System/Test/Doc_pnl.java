b99d18bdd5d6ed6a1fd0a501a9fbf39a68a87c84
B1ll-C <111455157+B1ll-C@users.noreply.github.com>
2022-12-22 16:53:45 +0800

    doc_pnl(update)

--- System/Test/Doc_pnl.java
+++ System/Test/Doc_pnl.java
@@ -9,21 +9,43 @@ import javax.swing.*;
 
 import java.awt.event.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
 
 
 
 
 public class Doc_pnl extends JPanel{
+
+	private static void cls(){
+		tbx_num.setText(null);
+		tbx_name.setText(null);
+		tbx_mail.setText(null);
+		tbx_m_y.setText(null);
+		tbx_cont.setText(null);
+	}
+
+	private static boolean is_empty(){
+		if(tbx_num.getText().isEmpty() || tbx_name.getText().isEmpty()|| tbx_m_y.getText().isEmpty() || tbx_cont.getText().isEmpty() || tbx_mail.getText().isEmpty()){
+			System.out.println("one is empty");
+			return false;
+		}
+	
+		return true;
+	}
 	public static JPanel Doc_pnl(){
 		JPanel pnl = new JPanel();
 		JPanel pnl_left = new JPanel();
 		JPanel pnl_main = new JPanel();
 
-		JTextPane tbx_num = new JTextPane();
-		JTextPane tbx_name = new JTextPane();
-		JTextPane tbx_mail = new JTextPane();
-		JTextPane tbx_m_y = new JTextPane();
-		JTextPane tbx_cont = new JTextPane();
+		// JTextPane tbx_num = new JTextPane();
+		// JTextPane tbx_name = new JTextPane();
+		// JTextPane tbx_mail = new JTextPane();
+		// JTextPane tbx_m_y = new JTextPane();
+		// JTextPane tbx_cont = new JTextPane();
+
+		JTextPane tbx_filt = new JTextPane();
+		JLabel lbl_filt = new JLabel("Search");
+
 
 		JLabel lbl_num = new JLabel("Number");
 		JLabel lbl_name = new JLabel("Name");
@@ -75,8 +97,15 @@ public class Doc_pnl extends JPanel{
 		lbl_cont.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
 		tbx_cont.setBounds(80,370,150,30);
 
+		int x = 80;
+		lbl_filt.setBounds(0,340+x+80,150,30);
+		lbl_filt.setFont(new Font("Barlow Condensed", Font.PLAIN, 25));
+		tbx_filt.setBounds(80,450+x,150,30);
 
 
+
+		pnl_left.add(lbl_filt);
+		pnl_left.add(tbx_filt);
 
 
 		pnl_left.add(tbx_num);
@@ -130,25 +159,117 @@ public class Doc_pnl extends JPanel{
 		model.addColumn("Email");
 		model.addColumn("Month & Year");
 		model.addColumn("Contact");
-
+		tab.setDefaultEditor(Object.class,null);
 		tab.setBounds(30,40,1054-500,646-50);
 		pnl_main.add(tab);
 		pnl_main.add(new JScrollPane(tab));
 
-
+		//remove this later
+		Object[] rows = {"1111","paul gonzalodo","@gmail","jan2022","909",};
+		model.addRow(rows);
+		Object[] rows1 = {"001","Dela Cruz","@yahoo","jan2022","102",};
+		model.addRow(rows1);
 
 
 		//event
 
 		btn_add.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				Object[] rows = {"me","me","me","me","me"};
-				model.addRow(rows);
+				if(is_empty()){
+					Object[] rows = {tbx_num.getText(),tbx_name.getText(),tbx_mail.getText(),tbx_m_y.getText(),tbx_cont.getText()};
+					model.addRow(rows);
+
+					cls();
+
+				}
+				
+				
 			}
 		});
-		
+
+		btn_update.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+
+
+				int result = JOptionPane.showConfirmDialog(pnl,"Update Data ?", "Caution",
+				JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE);
+
+				if(result == JOptionPane.YES_OPTION){
+					int i = tab.getSelectedRow();
+					if(i >= 0)
+					{
+						model.setValueAt(tbx_num.getText(), i, 0);
+						model.setValueAt(tbx_name.getText(), i, 1);
+						model.setValueAt(tbx_mail.getText(), i, 2);
+						model.setValueAt(tbx_m_y.getText(), i, 3);
+						model.setValueAt(tbx_cont.getText(), i, 4);
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "Error");
+					}
+				}
+
+				
+			}
+		});
+
+
+		btn_del.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				int result = JOptionPane.showConfirmDialog(pnl,"Delete Data ?", "Warning",
+				JOptionPane.YES_NO_OPTION,
+				JOptionPane.QUESTION_MESSAGE);
+
+				if(result == JOptionPane.YES_OPTION){
+					model.removeRow(tab.getSelectedRow());
+				}
+			}
+		});
+
+
+		tab.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+
+				String datas[] = new String[5];
+				int row = tab.getSelectedRow();
+
+				String value;
+				for(int i = 0; i < 5; i++){
+					datas[i]=tab.getModel().getValueAt(row, i).toString();
+				}
+
+
+				// System.out.println(datas[0]);
+				tbx_num.setText(datas[0]);
+				tbx_name.setText(datas[1]);
+				tbx_mail.setText(datas[2]);
+				tbx_m_y.setText(datas[3]);
+				tbx_cont.setText(datas[4]);
+			}
+		});
+    
+		tbx_filt.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent e){
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					System.out.println(tbx_filt.getText());
+  					TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(((DefaultTableModel) tab.getModel())); 
+  					// TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(((DefaultTableModel) tab.getModel()));
+					 sorter.setRowFilter(RowFilter.regexFilter(tbx_filt.getText()));
+
+				   tab.setRowSorter(sorter);
+					// final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+				}
+			}
+		});
 		return pnl;
 
 	}
+
+		private static JTextPane tbx_num = new JTextPane();
+		private static JTextPane tbx_name = new JTextPane();
+		private static JTextPane tbx_mail = new JTextPane();
+		private static JTextPane tbx_m_y = new JTextPane();
+		private static JTextPane tbx_cont = new JTextPane();
 
 }
