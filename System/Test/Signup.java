a26bd98366ded82f6446afabb0d20ed459bbdc3a
B1ll-C <111455157+B1ll-C@users.noreply.github.com>
2023-01-03 17:26:39 +0800

    update

---
+++ System/Test/Signup.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Signup extends JFrame{
+
+	public Signup(){
+
+	}
+
+	private void initComponents(){
+
+	}
+
+	
+	public static void main(String[] args){	
+		java.awt.EventQueue.invokeLater(new Runnable(){
+			public void run(){
+				try{
+					// Login login = new Login();
+					// login.setVisible(true);
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+		
+}
